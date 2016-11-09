fca167739f46061afb226e3f38bb01b9ba678785
Ronald Hong <ron@wistia.com>
2016-11-09 14:14:27 -0500

    Add test for source maps.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-const foo = 'bar';
+import Bugsnag from 'bugsnag-js';
+
+Bugsnag.apiKey = '46bd863e71b3bce9e377fd6243d63346';
+Bugsnag.notifyHandler = 'xhr';
+
+let foo = 'bar';
+const baz = 'qux';
+
+Bugsnag.notify("ErrorName", "Test Error");
+
+console.log('waeojraeorwjaejrow');
+
+throw new Error('testytest');
+
+foo = 'somethingelse';
+
+console.log('do stuff');
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  devtool: 'inline-source-map',
   entry: './index.js',
   output: {
     path: './',
